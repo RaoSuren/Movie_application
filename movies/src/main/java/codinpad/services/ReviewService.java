a404a0f3ca5250e172376dbcb68ff8d9a592265d
RaoSuren <surendraydv022@gmail.com>
2023-12-11 22:17:46 +0530

    Added Service and Data Layer Methods

---
+++ movies/src/main/java/codinpad/services/ReviewService.java
@@ -0,0 +1,34 @@
+package codinpad.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import codinpad.models.Movie;
+import codinpad.models.Review;
+import codinpad.repositories.ReviewRepository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+            .matching(Criteria.where("imdbId").is(imdbId))
+            .apply(new Update().push("reviews").value(review))
+            .first();
+
+        return review;
+    }
+}
