e8ef6d6ce2d79aed5f376cf85fb157dfdaacb54b
Yadav Kumar <125194075+RaoSuren@users.noreply.github.com>
2023-12-13 04:16:50 +0000

    Intializing of Front End part

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import logo from "./logo.svg";
+import "./App.css";
+import api from "./api/axiosConfig";
+import { useEffect, useState } from "react";
+import Layout from "./component/Layout";
+import { Routes, Route } from "react-router-dom";
+import Home from "./component/Home";
+
+function App() {
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    try {
+      const response = await api.get("/api/v1/movies");
+      setMovies(response.data);
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getMovies();
+  });
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
